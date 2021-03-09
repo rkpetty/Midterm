aa5a63d8aeaea02a40bdc9a4966dfbb693771271
rkpetty <rkpetty@umich.edu>
2021-03-09 12:31:17 -0500

    first branch commit

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,12 +2,17 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-        int first = 15;
-        int second = 25;
-
-        System.out.println("Enter two numbers: " + first + " " + second);
-        int sum = first + second;
+        int sum = AddInteger();
 
         System.out.println("The sum is: " + sum);
+    }
+
+    public int AddInteger(){
+      int first = 15;
+      int second = 25;
+
+      System.out.println("Enter two numbers: " + first + " " + second);
+      int sum = first + second;
+      return sum;
     }
 }
