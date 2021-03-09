67b0e10e48ce6f171a2e5759b5174cd26a425314
rkpetty <rkpetty@umich.edu>
2021-03-09 12:59:07 -0500

    Update AddIntegers.java

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,7 +2,7 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-      int first = 20;
+      int first = 40;
       int second = 15;
 
       System.out.println("Enter two numbers: " + first + " " + second);
