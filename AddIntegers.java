820e0d64493f3e01ab6828a7185d34da3480e959
rkpetty <rkpetty@umich.edu>
2021-03-09 12:56:43 -0500

    fix

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,17 +2,14 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-        int sum = AddInteger();
-
-        System.out.println("The sum is: " + sum);
-    }
-
-    public int AddInteger(){
       int first = 20;
       int second = 30;
 
       System.out.println("Enter two numbers: " + first + " " + second);
       int sum = first + second;
       return sum;
+
+        System.out.println("The sum is: " + sum);
     }
+
 }
